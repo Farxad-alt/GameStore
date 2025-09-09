3f68ec6701d2c3e124ac0a1c450eeec8513f5c6c
Farkhad <venzil05@gmail.com>
2025-09-09 15:27:24 +0300

    Добавление плагинов

---
+++ plugins/core-gamestore/core-gamestore.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name: Core GameStore
+ * Description: Основной плагин для темы GameStore.
+ * Version: 1.0.0
+ * Author: Farkhad
+ * Author URI: https://farhadsite.ru
+ * Text Domain: core-gamestore
+ * Domain Path: /languages
+ */
+
+define('GAMESTORE_PLUGIN_URL', plugin_dir_url(__FILE__));
+define('GAMESTORE_PLUGIN_PATH', plugin_dir_path(__FILE__));
